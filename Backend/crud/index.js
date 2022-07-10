de5f4ca4389376cc5d52e9b98df13dfb173812d7
Marcelo Chaves <Marcelochaves20000@gmail.com>
2022-07-10 16:09:26 -0300

    Initial commit

---
+++ Backend/crud/index.js
@@ -0,0 +1,39 @@
+// config inicial
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+
+// forma de ler JSON // middlewares
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+// Rotas da api
+const personRoutes = require('./routes/personRoutes')
+app.use('/person', personRoutes)
+
+// rota inicial / endpoint
+app.get("/", (req, res) => {
+
+  // mostrar req
+
+  res.json({message: 'hello world'})
+
+})
+
+
+// entregar uma porta
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kiphkfo.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+  console.log("Servidor conectado")
+  app.listen(3000)
+})
+.catch()
