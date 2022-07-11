39052b49347986dafecbbc82793520ce6d166a64
Marcelo Chaves <Marcelochaves20000@gmail.com>
2022-07-11 13:31:53 -0300

    feat: Improvements in home page

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Our team</h1>
+    <div class="home">
+      <section class="user-card" v-for="user in users" :key="user.id">
+        <img :src="user.avatar" :alt="user.name" />
+        <strong>{{ user.name }}</strong>
+        <span>{{ user.role }}</span>
+        <span>${{ user.salary }}(month)</span>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import api from "../services/api";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async created() {
+    const response = await api.get("/person");
+    this.users = response.data;
+  },
+};
 </script>
+
+<style>
+.home {
+  display: flex;
+  flex-wrap: wrap;
+}
+.user-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  border: 2px solid #aaa;
+  margin: 20px auto;
+  padding: 20px;
+  width: 300px;
+  max-width: 100%;
+  border-radius: 8px;
+}
+.user-card img {
+  width: 200px;
+  height: 200px;
+}
+</style>
